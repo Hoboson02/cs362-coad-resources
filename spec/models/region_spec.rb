89a2915d9c140fb1776704d1c17de45d7a2ce9c3
Ryan Earl <ryanleoearl@gmail.com>
2022-01-27 00:46:23 -0800

    Basic testing for region.rb

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
+  it "exists" do 
+    Region.new
+  end
+
+  describe "#to_s" do 
+    it "returns the name" do
+      name = 'My Hood'
+      region = Region.new(name: name)
+      expect(region.to_s).to eq(name)
+    end
+  end
+
+  describe "::unspecified" do 
+    it "returns a Region with the name 'Unspecified" do
+      region = Region.unspecified
+      expect(region.name).to eq('Unspecified')
+    end
+  end
 
 end
