70b8f7d600c0a8bac5cee23d4565c1e8eda93a0c
Doug Dennis <douglasdennisjr@gmail.com>
2022-01-27 11:41:57 -0800

    test existence of attributes in ticket #8

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -4,4 +4,47 @@ RSpec.describe Ticket, type: :model do
     it "exists" do
         Ticket.new
     end
+
+    describe "attributes" do
+        it "has a region" do
+            ticket = Ticket.new
+            expect(ticket).to respond_to(:region)
+        end
+
+        it "has a resource category" do
+            ticket = Ticket.new
+            expect(ticket).to respond_to(:resource_category)
+        end
+
+        it "has an organization" do
+            ticket = Ticket.new
+            expect(ticket).to respond_to(:organization)
+        end
+
+        it "has a name" do
+            ticket = Ticket.new
+            expect(ticket).to respond_to(:name)
+        end
+
+        it "has a phone" do
+            ticket = Ticket.new
+            expect(ticket).to respond_to(:phone)
+        end
+
+        it "has a description" do
+            ticket = Ticket.new
+            expect(ticket).to respond_to(:description)
+        end
+
+        it "has a closed" do
+            ticket = Ticket.new
+            expect(ticket).to respond_to(:closed)
+        end
+
+        it "has a closed_at" do
+            ticket = Ticket.new
+            expect(ticket).to respond_to(:closed_at)
+        end
+        
+    end
 end
