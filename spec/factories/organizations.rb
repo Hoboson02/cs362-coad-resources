dc39ebd4db52d686e75be7ffec8576ee908435e3
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-03 11:16:07 -0800

    added fake attributes to organization factory

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,4 +1,16 @@
 FactoryBot.define do
   factory :organization do
+    name { "Fake Name" }
+    status { 'submitted' }
+    phone { "15555555555" }
+    email { "fakeemail@fake.com" }
+    description { "Fake description." }
+    rejection_reason { nil }
+    liability_insurance { true }
+    primary_name { "Fake_primary_name" }
+    secondary_name { "Fake_secondary_name" }
+    secondary_phone { "15555555556" }
+    title { "FakeTitle" }
+    transportation { 'yes' }
   end
 end
