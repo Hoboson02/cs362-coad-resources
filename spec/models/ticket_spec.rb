a45ca585de5a9a727d083e23172ffb6cecfddfa8
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-03 13:24:33 -0800

    fix indentation in ticket_spec

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -4,85 +4,85 @@ RSpec.describe Ticket, type: :model do
   let(:ticket) { build(:ticket) }
   let(:closed_ticket) { build(:closed_ticket) }
 
-    describe "attributes" do
-        it "has a region" do
-            expect(ticket).to respond_to(:region)
-        end
-
-        it "has a resource category" do
-            expect(ticket).to respond_to(:resource_category)
-        end
-
-        it "has an organization" do
-            expect(ticket).to respond_to(:organization)
-        end
-
-        it "has a name" do
-            expect(ticket).to respond_to(:name)
-        end
-
-        it "has a phone" do
-            expect(ticket).to respond_to(:phone)
-        end
-
-        it "has a description" do
-            expect(ticket).to respond_to(:description)
-        end
-
-        it "has a closed" do
-            expect(ticket).to respond_to(:closed)
-        end
-
-        it "has a closed_at" do
-            expect(ticket).to respond_to(:closed_at)
-        end     
+  describe "attributes" do
+    it "has a region" do
+      expect(ticket).to respond_to(:region)
     end
 
-    describe "associations" do
-        it { should belong_to(:region) }
-        it { should belong_to(:resource_category) }
-        it { should belong_to(:organization) }
+    it "has a resource category" do
+      expect(ticket).to respond_to(:resource_category)
     end
 
-    describe "validations" do
-        it { should validate_presence_of(:name) }
-        it { should validate_presence_of(:phone) }
-        it { should validate_presence_of(:region_id) }
-        it { should validate_presence_of(:resource_category_id) }
-        it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
-        it { should validate_length_of(:description).is_at_most(1020).on(:create) }
+    it "has an organization" do
+      expect(ticket).to respond_to(:organization)
     end
 
-    describe "#open?" do
-        it "is open when it hasn't been closed" do
-            expect(ticket.closed).to eq(false)
-            expect(ticket.open?).to eq(true)
-        end
-
-        it "is not open when it has been closed" do
-            expect(closed_ticket.closed).to eq(true)
-            expect(closed_ticket.open?).to eq(false)
-        end
+    it "has a name" do
+      expect(ticket).to respond_to(:name)
     end
 
-    describe "#captured?" do
-        it "is not captured when an organization is not present" do
-            expect(ticket.organization.present?).to eq(false)
-            expect(ticket.captured?).to eq(false)
-        end
-
-        it "is captured when an organization is present" do
-            expect(ticket.captured?).to eq(false)
-            ticket.organization = Organization.new
-            expect(ticket.captured?).to eq(true)
-        end
+    it "has a phone" do
+      expect(ticket).to respond_to(:phone)
     end
 
-    describe "#to_s" do
-        it "returns a string identifying the ticket number" do
-            expected_result = "Ticket -1"
-            ticket.id = -1
-            expect(ticket.to_s).to eq(expected_result)
-        end
+    it "has a description" do
+      expect(ticket).to respond_to(:description)
     end
+
+    it "has a closed" do
+      expect(ticket).to respond_to(:closed)
+    end
+
+    it "has a closed_at" do
+      expect(ticket).to respond_to(:closed_at)
+    end     
+  end
+
+  describe "associations" do
+    it { should belong_to(:region) }
+    it { should belong_to(:resource_category) }
+    it { should belong_to(:organization) }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:phone) }
+    it { should validate_presence_of(:region_id) }
+    it { should validate_presence_of(:resource_category_id) }
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+    it { should validate_length_of(:description).is_at_most(1020).on(:create) }
+  end
+
+  describe "#open?" do
+    it "is open when it hasn't been closed" do
+      expect(ticket.closed).to eq(false)
+      expect(ticket.open?).to eq(true)
+    end
+
+    it "is not open when it has been closed" do
+      expect(closed_ticket.closed).to eq(true)
+      expect(closed_ticket.open?).to eq(false)
+    end
+  end
+
+  describe "#captured?" do
+    it "is not captured when an organization is not present" do
+      expect(ticket.organization.present?).to eq(false)
+      expect(ticket.captured?).to eq(false)
+    end
+
+    it "is captured when an organization is present" do
+      expect(ticket.captured?).to eq(false)
+      ticket.organization = Organization.new
+      expect(ticket.captured?).to eq(true)
+    end
+  end
+
+  describe "#to_s" do
+    it "returns a string identifying the ticket number" do
+      expected_result = "Ticket -1"
+      ticket.id = -1
+      expect(ticket.to_s).to eq(expected_result)
+    end
+  end
 end
