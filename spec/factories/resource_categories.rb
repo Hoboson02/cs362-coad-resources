a576f2df3a1f8058706cd83d5138fe5e8e378db1
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-04 21:55:02 -0800

    created inactive_resource_category factory #23

--- spec/factories/resource_categories.rb
+++ spec/factories/resource_categories.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :resource_category do
     name { "Fake Resource Category" }
     active { true }
+
+    factory :inactive_resource_category do
+      active { false }
+    end
   end
 end
