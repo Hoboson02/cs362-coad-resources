5acb49807fabe576990ec1d1c3bdba1fc0127aef
Ryan Earl <ryanleoearl@gmail.com>
2022-02-06 23:19:25 -0800

    Testing of validators added to region_spec
    References #14

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -20,4 +20,11 @@ RSpec.describe Region, type: :model do
     end
   end
 
+  describe "validations" do
+    it { should validate_presence_of(:name)}
+    it { should validate_length_of(:name)}
+    it { should validate_uniqueness_of(:name)}
+
+  end
+
 end
