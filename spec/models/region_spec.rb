d1a2ab1c970073058c2b8063b0aaa2812dbf559a
Ryan Earl <ryanleoearl@gmail.com>
2022-02-08 00:05:25 -0800

    Made the validation for name in region_spec case insensitiveto fix earlier failure

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Region, type: :model do
   describe "validations" do
     it { should validate_presence_of(:name)}
     it { should validate_length_of(:name)}
-    it { should validate_uniqueness_of(:name)}
+    it { should validate_uniqueness_of(:name).case_insensitive }
 
   end
 
