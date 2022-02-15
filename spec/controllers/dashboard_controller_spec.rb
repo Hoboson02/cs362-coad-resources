7f31e0c5bf3db9621a6f54ce06c6d1c1c501b7d6
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-15 13:13:13 -0800

    added dashboard controller test for redirect #26

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-
+  it "redirects to login when not signed in" do
+    get :index
+    expect(response).to be_redirect
+    expect(response).to redirect_to("/users/sign_in")
+  end
 end
