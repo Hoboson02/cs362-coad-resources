9a9aac068ed102098910823bfa5fe2e5e4aac215
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-22 12:05:43 -0800

    resource categories controller tests for organization user #31

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -26,4 +26,30 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to redirect_to(new_user_session_path)
     end
   end
+
+  describe "organization user" do
+    it "redirected to dashboard" do
+      sign_in(create(:organization_user))
+      get :index
+      expect(response).to redirect_to(dashboard_path)
+      get :show, params: {id: "FAKE"}
+      expect(response).to redirect_to(dashboard_path)
+      get :new
+      expect(response).to redirect_to(dashboard_path)
+      post :create
+      expect(response).to redirect_to(dashboard_path)
+      get :edit, params: {id: "FAKE"}
+      expect(response).to redirect_to(dashboard_path)
+      patch :update, params: {id: "FAKE"}
+      expect(response).to redirect_to(dashboard_path)
+      put :update, params: {id: "FAKE"}
+      expect(response).to redirect_to(dashboard_path)
+      patch :activate, params: {id: "FAKE"}
+      expect(response).to redirect_to(dashboard_path)
+      patch :deactivate, params: {id: "FAKE"}
+      expect(response).to redirect_to(dashboard_path)
+      delete :destroy, params: {id: "FAKE"}
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
 end
