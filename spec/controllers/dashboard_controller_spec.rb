8f1b291be7017b7fea8e21b6cd3375b1786c55a8
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-22 10:49:52 -0800

    added describe block

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-  it "redirects to login when not signed in" do
-    get :index
-    expect(response).to redirect_to(new_user_session_path)
+  describe "a user that is not signed in" do
+    it "redirects to sign in" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+    end
   end
 end
