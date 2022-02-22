24e099bada3b16ab88ee18b3590fe18f095a5f4f
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-22 11:46:17 -0800

    added controller test helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -98,4 +98,8 @@ RSpec.configure do |config|
 
   config.include(Shoulda::Matchers::ActiveModel, type: :model)
   config.include(Shoulda::Matchers::ActiveRecord, type: :model)
+
+  RSpec.configure do |config|
+    config.include Devise::Test::ControllerHelpers, :type => :controller
+  end
 end
