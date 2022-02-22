8725b91a3d99f3c336262eed066c54d0cf7e52b9
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-22 10:33:39 -0800

    removed low value expectation

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe DashboardController, type: :controller do
   it "redirects to login when not signed in" do
     get :index
-    expect(response).to be_redirect
-    expect(response).to redirect_to("/users/sign_in")
+    expect(response).to redirect_to(new_user_session_path)
   end
 end
