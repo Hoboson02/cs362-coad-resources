4d33d299217bbbc7306d7c8e9f716d4f4965f520
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-22 11:47:31 -0800

    added confirmation after create, created organization_user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     email { "testemail@mail.com" }
     password { "thisisapassword" }
     role { "admin" }
+    
+    after :create, &:confirm
+
+    factory :organization_user do
+      role { "organization" }
+    end
   end
 end
