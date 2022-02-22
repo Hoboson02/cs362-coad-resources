8490293e717d2cb2fbeb7f0269950e3de02ff418
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-22 11:23:06 -0800

    not signed user controller test for organizations #29

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe OrganizationsController, type: :controller do
-
+  describe "user that is not signed in" do
+    it "redirected to sign in" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+      post :index
+      expect(response).to redirect_to(new_user_session_path)
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+      post :create
+      expect(response).to redirect_to(new_user_session_path)
+      get :edit, params: {id: "FAKE"}
+      expect(response).to redirect_to(new_user_session_path)
+      patch :update, params: {id: "FAKE"}
+      expect(response).to redirect_to(new_user_session_path)
+      put :update, params: {id: "FAKE"}
+      expect(response).to redirect_to(new_user_session_path)
+      get :show, params: {id: "FAKE"}
+      expect(response).to redirect_to(new_user_session_path)
+      post :approve, params: {id: "FAKE"}
+      expect(response).to redirect_to(new_user_session_path)
+      post :reject, params: {id: "FAKE"}
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
 end
