55c66b59cddbfe062800b99f41388b67615929cc
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-24 11:31:22 -0800

    created tickets controller tests

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
+  describe "user that is not signed in" do
+    it "redirected to sign in" do
+      delete :destroy, params: {id: "FAKE"}
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
 
+  describe "organization user" do
+    it "redirected to dashboard" do
+      sign_in(create(:organization_user))
+      delete :destroy, params: {id: "FAKE"}
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
 end
