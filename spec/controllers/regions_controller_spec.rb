cd6b6cfd43220c6faa75c2478e0a32b6276c3ecd
Ryan Earl <ryanleoearl@gmail.com>
2022-02-27 23:25:59 -0800

    Completion of regions_controller_spec
    References #30

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -2,4 +2,46 @@ require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
 
+  describe "a non logged in person" do
+    it "redirects to the sign in screen" do
+      get :index
+      expect(response).to redirect_to(new_user_session_path)
+      get :show, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+      get :create
+      expect(response).to redirect_to(new_user_session_path)
+      get :edit, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      get :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      get :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+      get :destroy, params: {id: 'FAKE'}
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+  describe "a organization user" do
+    it "redirects to the sign in screen" do
+      sign_in(create(:organization_user))
+      get :index
+      expect(response).to redirect_to(dashboard_path)
+      get :show, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      get :new
+      expect(response).to redirect_to(dashboard_path)
+      post :create
+      expect(response).to redirect_to(dashboard_path)
+      get :edit, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      patch :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      put :update, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+      delete :destroy, params: {id: 'FAKE'}
+      expect(response).to redirect_to(dashboard_path)
+    end
+  end
+
 end
