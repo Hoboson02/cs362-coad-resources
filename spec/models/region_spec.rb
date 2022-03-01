32e7e542c425108224dbf360dd1af8550ca423aa
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-28 18:50:15 -0800

    linting region_spec #27

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -3,22 +3,22 @@ require 'rails_helper'
 RSpec.describe Region, type: :model do
   let(:region) { build(:region) }
 
-  describe "#to_s" do 
-    it "returns the name" do
+  describe '#to_s' do
+    it 'returns the name' do
       expect(region.to_s).to eq(region.name)
     end
   end
 
-  describe "::unspecified" do 
-    it "returns a Region with the name 'Unspecified" do
+  describe '::unspecified' do
+    it 'returns a Region with the name "Unspecified"' do
       region = Region.unspecified
       expect(region.name).to eq('Unspecified')
     end
   end
 
-  describe "validations" do
-    it { should validate_presence_of(:name)}
-    it { should validate_length_of(:name)}
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name) }
     it { should validate_uniqueness_of(:name).case_insensitive }
   end
 end
