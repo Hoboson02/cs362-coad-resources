94a51acca87f012a5c68f798aecd4335e0a50711
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-28 18:20:00 -0800

    linting for resource_categories_controller_spec #27

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,54 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
-
-  describe "user that is not signed in" do
-    it "redirected to sign in" do
+  describe 'user that is not signed in' do
+    it 'redirected to sign in' do
       get :index
       expect(response).to redirect_to(new_user_session_path)
-      get :show, params: {id: "FAKE"}
+      get :show, params: { id: 'FAKE' }
       expect(response).to redirect_to(new_user_session_path)
       get :new
       expect(response).to redirect_to(new_user_session_path)
       post :create
       expect(response).to redirect_to(new_user_session_path)
-      get :edit, params: {id: "FAKE"}
+      get :edit, params: { id: 'FAKE' }
       expect(response).to redirect_to(new_user_session_path)
-      patch :update, params: {id: "FAKE"}
+      patch :update, params: { id: 'FAKE' }
       expect(response).to redirect_to(new_user_session_path)
-      put :update, params: {id: "FAKE"}
+      put :update, params: { id: 'FAKE' }
       expect(response).to redirect_to(new_user_session_path)
-      patch :activate, params: {id: "FAKE"}
+      patch :activate, params: { id: 'FAKE' }
       expect(response).to redirect_to(new_user_session_path)
-      patch :deactivate, params: {id: "FAKE"}
+      patch :deactivate, params: { id: 'FAKE' }
       expect(response).to redirect_to(new_user_session_path)
-      delete :destroy, params: {id: "FAKE"}
+      delete :destroy, params: { id: 'FAKE' }
       expect(response).to redirect_to(new_user_session_path)
     end
   end
 
-  describe "organization user" do
-    it "redirected to dashboard" do
+  describe 'organization user' do
+    it 'redirected to dashboard' do
       sign_in(create(:organization_user))
       get :index
       expect(response).to redirect_to(dashboard_path)
-      get :show, params: {id: "FAKE"}
+      get :show, params: { id: 'FAKE' }
       expect(response).to redirect_to(dashboard_path)
       get :new
       expect(response).to redirect_to(dashboard_path)
       post :create
       expect(response).to redirect_to(dashboard_path)
-      get :edit, params: {id: "FAKE"}
+      get :edit, params: { id: 'FAKE' }
       expect(response).to redirect_to(dashboard_path)
-      patch :update, params: {id: "FAKE"}
+      patch :update, params: { id: 'FAKE' }
       expect(response).to redirect_to(dashboard_path)
-      put :update, params: {id: "FAKE"}
+      put :update, params: { id: 'FAKE' }
       expect(response).to redirect_to(dashboard_path)
-      patch :activate, params: {id: "FAKE"}
+      patch :activate, params: { id: 'FAKE' }
       expect(response).to redirect_to(dashboard_path)
-      patch :deactivate, params: {id: "FAKE"}
+      patch :deactivate, params: { id: 'FAKE' }
       expect(response).to redirect_to(dashboard_path)
-      delete :destroy, params: {id: "FAKE"}
+      delete :destroy, params: { id: 'FAKE' }
       expect(response).to redirect_to(dashboard_path)
     end
   end
