189c1e363aa145dcd10ca6957d3402a5b930afd9
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-28 18:19:12 -0800

    linting for dashboard_controller_spec #27

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-  describe "a user that is not signed in" do
-    it "redirects to sign in" do
+  describe 'a user that is not signed in' do
+    it 'redirects to sign in' do
       get :index
       expect(response).to redirect_to(new_user_session_path)
     end
