67b752f642a93db88982281b5226827eb69aed75
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-28 17:10:26 -0800

    added sequence salt to organization factory

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :organization do
-    name { "Fake Name" }
+    sequence(:name) { |n| "Fake Name#{n}" }
     status { 'submitted' }
     phone { "15555555555" }
-    email { "fakeemail@fake.com" }
+    sequence(:email) { |n| "fakeemail#{n}@fake.com" }
     description { "Fake description." }
     rejection_reason { nil }
     liability_insurance { true }
