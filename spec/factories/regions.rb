5c67d8975b02859aaecb53c34bde63977c1faebe
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-28 18:25:15 -0800

    linting for regions factory #27

--- spec/factories/regions.rb
+++ spec/factories/regions.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :region do
-        sequence(:name) { |n| "John Doe#{n}" }
+    sequence(:name) { |n| "John Doe#{n}" }
 
-        factory :without_name do 
-            name { nil }
-        end
+    factory :without_name do
+      name { nil }
     end
+  end
 end
