057a31b66382d52fa29884edcaa2a2c020e7894d
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-28 18:25:02 -0800

    linting for orangizations factories #27

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -2,15 +2,15 @@ FactoryBot.define do
   factory :organization do
     sequence(:name) { |n| "Fake Name#{n}" }
     status { 'submitted' }
-    phone { "15555555555" }
+    phone { '15555555555' }
     sequence(:email) { |n| "fakeemail#{n}@fake.com" }
-    description { "Fake description." }
+    description { 'Fake description.' }
     rejection_reason { nil }
     liability_insurance { true }
-    primary_name { "Fake_primary_name" }
-    secondary_name { "Fake_secondary_name" }
-    secondary_phone { "15555555556" }
-    title { "FakeTitle" }
+    primary_name { 'Fake_primary_name' }
+    secondary_name { 'Fake_secondary_name' }
+    secondary_phone { '15555555556' }
+    title { 'FakeTitle' }
     transportation { 'yes' }
   end
 end
