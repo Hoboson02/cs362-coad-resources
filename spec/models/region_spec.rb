b3eedae32a64a440a6a83716bb6d70228d9bd135
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-28 17:16:57 -0800

    updated spec to use factory

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,15 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
-  it "exists" do 
-    Region.new
-  end
+  let(:region) { build(:region) }
 
   describe "#to_s" do 
     it "returns the name" do
-      name = 'My Hood'
-      region = Region.new(name: name)
-      expect(region.to_s).to eq(name)
+      expect(region.to_s).to eq(region.name)
     end
   end
 
@@ -24,7 +20,5 @@ RSpec.describe Region, type: :model do
     it { should validate_presence_of(:name)}
     it { should validate_length_of(:name)}
     it { should validate_uniqueness_of(:name).case_insensitive }
-
   end
-
 end
