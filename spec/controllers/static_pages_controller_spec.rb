7c4abe64800e421754e031bb78ec389763d6a9bb
Doug Dennis <douglasdennisjr@gmail.com>
2022-02-28 19:51:39 -0800

    added controller test for static pages #33

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-
+  describe 'user that is not signed in' do
+    it 'does not redirect' do
+      get :index
+      expect(response.redirect?).to be(false)
+    end
+  end
 end
