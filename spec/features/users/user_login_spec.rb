f6b6e99e222851ed3cb63b313e7b453597b08325
Doug Dennis <douglasdennisjr@gmail.com>
2022-03-10 19:22:32 -0800

    test for incorrect login #43

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -22,4 +22,12 @@ RSpec.describe 'Logging in', type: :feature do
     expect(page).to have_text 'Signed in successfully'
     expect(current_path).to eq('/dashboard')
   end
+
+  scenario "with incorrect credentials causes an error to display" do
+    visit '/login'
+    fill_in 'Email address', with: "wrongemail@bad.com"
+    fill_in 'Password', with: "wrongpassword"
+    click_on 'Sign in'
+    expect(page).to have_text 'Invalid Email or password.'
+  end 
 end
