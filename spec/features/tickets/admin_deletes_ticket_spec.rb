11c00935914993526362fd3b1d96999d793b9b5c
Doug Dennis <douglasdennisjr@gmail.com>
2022-03-10 18:52:33 -0800

    feature test for deleting a ticket #42

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
-
+  scenario "is successful when user is an admin" do
+    user = create(:user)
+    ticket = create(:ticket)
+    log_in_as(user)
+    
+    visit "/tickets/#{ticket.id}"
+    click_on "Delete"
+    expect(page).to have_text "Ticket #{ticket.id} was deleted."
+    expect{ Ticket.find(ticket.id) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
