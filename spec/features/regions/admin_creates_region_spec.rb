d60cc0218c5a558e14584d2ab64b71c57f7ed65a
Doug Dennis <douglasdennisjr@gmail.com>
2022-03-10 18:40:44 -0800

    test for error displayed when creating region with duplicate name #41

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe 'Creating a Region', type: :feature do
     expect(page).to have_text "Region successfully created."
     expect{Region.find_by(name: "Fake Region")}.not_to raise_error
   end
+
+  scenario "with a duplicate name is not successful" do
+    user = create(:user)
+    region = create(:region)
+    log_in_as(user)
+
+    visit "/regions"
+    click_on "Add Region"
+    fill_in "Name", with: region.name
+    click_on "Add Region"
+    expect(page).to have_text "error prohibited this region from being saved"
+    expect(page).to have_text "Name has already been taken"
+  end
 end
