659ece788f2528d47b165d71696dd5c2e4a9af75
Doug Dennis <douglasdennisjr@gmail.com>
2022-03-10 19:22:11 -0800

    test for organization user logging in #43

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe 'Logging in', type: :feature do
     expect(page).to have_text "Signed in successfully"
     expect(current_path).to eq('/dashboard')
   end
+
+  scenario "organization user logging in goes to dashboard" do
+    user = create(:organization_user)
+
+    visit '/login'
+    fill_in 'Email address', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Sign in'
+    expect(page).to have_text 'Signed in successfully'
+    expect(current_path).to eq('/dashboard')
+  end
 end
