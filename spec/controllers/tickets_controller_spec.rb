4ab7fb3c6d3f3e38ea6ec4153ee2aea5b0de2ecb
Doug Dennis <douglasdennisjr@gmail.com>
2022-03-10 17:13:37 -0800

    added missed routes to tickets controller spec for not signed in user #39

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -2,8 +2,20 @@ require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
   describe 'user that is not signed in' do
-    it 'redirected to sign in' do
+    it 'redirected to dashboard' do
       delete :destroy, params: { id: 'FAKE' }
+      expect(response).to redirect_to(dashboard_path)
+
+      get :show, params: { id: 'FAKE' }
+      expect(response).to redirect_to(dashboard_path)
+
+      post :capture, params: { id: 'FAKE' }
+      expect(response).to redirect_to(dashboard_path)
+
+      post :release, params: { id: 'FAKE' }
+      expect(response).to redirect_to(dashboard_path)
+
+      patch :close, params: { id: 'FAKE' }
       expect(response).to redirect_to(dashboard_path)
     end
   end
