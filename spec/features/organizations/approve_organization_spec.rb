ec0bda02aa2835c379e667624dbb26482b19c450
Doug Dennis <douglasdennisjr@gmail.com>
2022-03-10 17:32:26 -0800

    basic feature working #40

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
-
+  it "approves an organization" do
+    user = create(:user)
+    organization = create(:organization)
+    log_in_as(user)
+    visit "/organizations/#{organization.id}"
+    click_on "Approve"
+    expect(page).to have_text "Organization #{organization.name} has been approved."
+  end
 end
