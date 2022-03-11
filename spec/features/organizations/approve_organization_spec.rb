ef54e544b64d5d538978e794b9db23430fcbfaaf
Doug Dennis <douglasdennisjr@gmail.com>
2022-03-10 17:45:06 -0800

    added expectations for organization in approval feature #40

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
-  scenario "approves a pending organization" do
+  scenario 'approves a pending organization' do
     user = create(:user)
     organization = create(:organization)
+    
+    expect(organization).to_not be_approved
+    
     log_in_as(user)
     visit "/organizations/#{organization.id}"
-    click_on "Approve"
+    click_on 'Approve'
     expect(page).to have_text "Organization #{organization.name} has been approved."
+
+    organization.reload
+    expect(organization).to be_approved
   end
 end
