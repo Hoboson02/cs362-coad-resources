0432262af298a1e427b297dcc375b0a123755fc4
Doug Dennis <douglasdennisjr@gmail.com>
2022-03-10 18:35:40 -0800

    feature test for adding succesful region #41

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
-
+  scenario "with correct values is succesful" do
+    user = create(:user)
+    log_in_as(user)
+    visit "/regions"
+    click_on "Add Region"
+    fill_in "Name", with: "Fake Region"
+    click_on "Add Region"
+    expect(page).to have_text "Region successfully created."
+    expect{Region.find_by(name: "Fake Region")}.not_to raise_error
+  end
 end
