f86b9bf2968c96ada9a381d715a7a80d313015b3
Doug Dennis <douglasdennisjr@gmail.com>
2022-03-10 19:01:52 -0800

    feature test for admin user logging in #43

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+  scenario "admin user logging in goes to dashboard" do
+    user = create(:user)
 
+    visit "/login"
+    fill_in "Email address", with: user.email
+    fill_in "Password", with: user.password
+    click_on "Sign in"
+    expect(page).to have_text "Signed in successfully"
+    expect(current_path).to eq('/dashboard')
+  end
 end
