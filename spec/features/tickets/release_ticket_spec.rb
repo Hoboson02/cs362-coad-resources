16d1c93ba478bd375cfb621257cba97b663da0ab
Doug Dennis <douglasdennisjr@gmail.com>
2022-03-13 13:33:23 -0700

    added test for admin user successfully releasing ticket #52

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Releasing a ticket by an', type: :feature do
+  scenario "admin user is successful" do
+    user = create(:user)
+    ticket = create(:ticket)
+    organization = create(:organization)
 
+    ticket.organization = organization
+    ticket.save
+
+    expect(ticket).to be_captured
+
+    log_in_as(user)
+    visit("/tickets/#{ticket.id}")
+    click_on 'Release'
+    
+    ticket.reload
+    expect(ticket).to_not be_captured
+  end
 end
