bd7e1d767e0347ca7aecb109d3046137cea36e67
Ryan Earl <ryanleoearl@gmail.com>
2022-03-13 05:52:48 -0700

    Completion of create_ticket feature test #51

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
+  scenario "organization user is looking to get help" do
+    approved_organization = create(:organization, status: :approved)
+    region = create(:region)
+    resource_category = create(:resource_category)
+    user = build(:user, role: :organization, email: "test@test.com", password: "password", organization: approved_organization)
+    user.confirm
+    log_in_as(user)
 
+    visit "/tickets/new"
+
+    fill_in "ticket_name", with: "John Doe"
+    fill_in "ticket_phone", with: "5555555555"
+    select "John Doe", from: "ticket_region_id"
+    select "Fake Resource Category", from: "ticket_resource_category_id"
+    fill_in "ticket_description", with: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    click_on "Send this help request"
+    expect(page).to have_text "Thank you for submitting"
+  end
 end
