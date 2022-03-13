f4fb28de8a9aeea4582fcc1e635314bf57f05020
Ryan Earl <ryanleoearl@gmail.com>
2022-03-13 01:21:33 -0800

    Skeleton of create_organization_application_spec implemented #44

--- spec/features/organizations/create_organization_application_spec.rb
+++ spec/features/organizations/create_organization_application_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Creating an Organization Application', type: :feature do
+  scenario "user is applying" do
+    skip
+    user = create(:user)
+    user.email = "test@test.com"
+    user.password = "password"
+    user.organization = nil
+    user.role = nil
+    user.confirm
+    user.save!
+    log_in_as(user)
 
+
+    visit "/new_organization_application"
+  
+    choose "organization[]"  
+    choose "organization[]"  
+    choose "organization[]"
+    choose "organization[]"
+    choose "organization[]"
+    choose "organization[]"
+    choose "organization[]"
+    choose "organization[]"  
+    choose "organization[]"
+    fill_in "organization"
+    fill_in "organization"
+    fill_in "organization"
+    fill_in "organization"
+    fill_in "organization"
+    fill_in "organization"
+    fill_in "organization"
+    fill_in "organization"  
+
+    click_on "Apply"
+
+    expect(page).to have_text("Thank you for applying.")
+  end
 end
--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
-
 end
