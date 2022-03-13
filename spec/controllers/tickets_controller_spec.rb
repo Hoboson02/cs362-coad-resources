76c8be2c7fafcd4d57b089c82838f58dfef5a6d4
Doug Dennis <douglasdennisjr@gmail.com>
2022-03-12 19:12:27 -0800

    added missing tickets controller tests for organization users #39

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -22,7 +22,33 @@ RSpec.describe TicketsController, type: :controller do
 
   describe 'organization user' do
     it 'redirected to dashboard' do
+      user = create(:organization_user)
+      user.organization = create(:approved_organization)
+      sign_in(user)
+
+      delete :destroy, params: { id: 'FAKE' }
+      expect(response).to redirect_to(dashboard_path)
+    end
+
+    it 'unapproved redirected to dashboard' do
+      user = create(:organization_user)
+      user.organization = create(:organization)
       sign_in(create(:organization_user))
+
+      expect(user.organization).to_not be_approved
+
+      get :show, params: { id: 'FAKE' }
+      expect(response).to redirect_to(dashboard_path)
+
+      post :capture, params: { id: 'FAKE' }
+      expect(response).to redirect_to(dashboard_path)
+
+      post :release, params: { id: 'FAKE' }
+      expect(response).to redirect_to(dashboard_path)
+
+      patch :close, params: { id: 'FAKE' }
+      expect(response).to redirect_to(dashboard_path)
+
       delete :destroy, params: { id: 'FAKE' }
       expect(response).to redirect_to(dashboard_path)
     end
