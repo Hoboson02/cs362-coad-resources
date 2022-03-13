760395ebd0a4c88d53d99d0b1cc6e1186c3ad9ad
Ryan Earl <ryanleoearl@gmail.com>
2022-03-13 04:59:02 -0700

    Completion of admin_deletes_region feature test #47

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -1,4 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
+  let(:admin) { create(:user, role: :admin, email: "admin_test@test.com", password: "password") }
+  scenario "as an administrative user viewing regions" do
+    create(:region)
+    log_in_as(admin)
+    visit "/regions"
+    visit "/regions/1"
+    click_on "Delete"
+    expect(page).to have_text "Associated tickets"
+  end
 end
