3d11fabaa9e831ee657487a0d3f88d4f42aae824
Ryan Earl <ryanleoearl@gmail.com>
2022-03-13 05:23:12 -0700

    Completion of capture_ticket feature test #49

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
+  scenario "as a user viewing tickets" do
+    approved_organization = create(:organization, status: :approved)
+    tickets = create(:ticket)
+    user = build(:user, role: :organization, email: "test@test.com", password: "password", organization: approved_organization)
+    user.confirm
+    log_in_as(user)
 
+    visit "/dashboard"
+ 
+    click_on "Tickets"
+
+    find(:xpath, "//a[@href='/tickets/1']").click
+
+    click_on "Capture"
+
+  end
 end
