d7fbcfda19d03c784660191b741aaa42947d826a
Ryan Earl <ryanleoearl@gmail.com>
2022-03-13 04:12:01 -0700

    Completion of reject_organization feature test #45

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
-
+  let(:admin) { create(:user, role: :admin, email: "admin_test@test.com", password: "password") }
+  scenario "as an administrative user viewing applications" do
+    admin.confirm
+    create(:organization)
+    log_in_as(admin)
+    visit "/organizations"
+    click_on "Pending"
+    click_on "Review"
+    expect(page).to have_content("I own and am responsible")
+    click_on "Reject"
+    expect(page).to have_content("has been rejected")
+  end
 end
