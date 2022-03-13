e4fd839d3aec8c6da469f072dff12fcfc5db26e4
Ryan Earl <ryanleoearl@gmail.com>
2022-03-13 04:42:27 -0700

    Completion of update_organization feature test #46

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
+  scenario "user is editing their organization" do
+    approved_organization = create(:organization, status: :approved)
+    user = build(:user, role: :organization, email: "test@test.com", password: "password", organization: approved_organization)
+    user.confirm
+    log_in_as(user)
 
+    visit "/dashboard"
+
+    click_on "Edit Organization"
+
+    click_on "Update Resource"
+  end
 end
