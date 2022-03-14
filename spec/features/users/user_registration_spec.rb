dc2b963074f3cf057bad43268c9f941b97f31a74
Ryan Earl <ryanleoearl@gmail.com>
2022-03-13 23:13:20 -0700

    Completion of user_registration feature test #53

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
+  scenario "as a potential new user" do
+    visit "/signup"
+    fill_in 'user_email', with: "testing@test.com"
+    fill_in 'user_password', with: "passw0rd"
+    fill_in 'user_password_confirmation', with: "passw0rd"
+    click_on "commit"
+
+    expect(page).to have_text "Please follow the link to activate your account"
+  end
 
 end
