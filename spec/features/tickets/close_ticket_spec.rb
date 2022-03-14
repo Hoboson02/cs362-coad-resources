f21b80afa4c7556b79d8f59096ef7ed9c4be20ed
Ryan Earl <ryanleoearl@gmail.com>
2022-03-13 22:57:09 -0700

    Completion of close_ticket feature test #50

--- spec/features/tickets/close_ticket_spec.rb
+++ spec/features/tickets/close_ticket_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Closing a ticket', type: :feature do
+  scenario "as a signed in organization user who has already captured the ticket" do
+    approved_organization = create(:organization, status: :approved)
+    tickets = create(:ticket)
+    user = build(:user, role: :organization, email: "test@test.com", password: "password", organization: approved_organization)
+    user.confirm
+    log_in_as(user)
 
+    visit "/dashboard"
+ 
+    click_on "Tickets"
+
+    find(:xpath, "//a[@href='/tickets/1']").click
+    click_on "Capture"
+
+    find(:xpath, "//a[@href='/tickets/1']").click
+    click_on "Close"
+
+    find(:xpath, "//a[@href='/tickets/1']").click
+    expect(page).to have_text("Closed")
+    
+  end
 end
